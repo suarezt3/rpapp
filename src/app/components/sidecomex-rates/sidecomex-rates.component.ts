6f6baf1bc344bcb50eb33086dc3523864c26d6ec
Eyder Suarez <ey_der19@hotmail.com>
2023-09-08 11:24:34 -0500

    Se comienza el componente de sidecomex

--- src/app/components/sidecomex-rates/sidecomex-rates.component.ts
+++ src/app/components/sidecomex-rates/sidecomex-rates.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sidecomex-rates',
   templateUrl: './sidecomex-rates.component.html',
   styleUrls: ['./sidecomex-rates.component.css']
 })
-export class SidecomexRatesComponent {
+export class SidecomexRatesComponent implements OnInit {
+
+  public myForm!: FormGroup;
+
+  constructor (private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      concept: ["",[]],
+      value  : ["",[]]
+    })
+  }
 
 }
