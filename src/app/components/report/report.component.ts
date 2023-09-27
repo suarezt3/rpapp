a7f9620ddb2989a2e2da45f8f83aef982dcfd097
Eyder Suarez <ey_der19@hotmail.com>
2023-09-27 16:24:59 -0500

    se avanza en el search

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MATERIAL } from 'src/app/interfaces/codigoMaterial';
 import { DataService } from 'src/app/services/data.service';
 
@@ -10,15 +11,29 @@ import { DataService } from 'src/app/services/data.service';
 export class ReportComponent implements OnInit {
 
   public data!: MATERIAL[];
+  public formSearch!: FormGroup
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.dataService.getCodigoMaterial().subscribe((resp: MATERIAL[]) => {
       this.data = resp
       console.log(this.data);
-
     })
+
+    this.formSearch = this.fb.group({
+      search: ["".toUpperCase()]
+    })
+
+
   }
+
+  enviar() {
+    console.log(this.formSearch.get('search')?.value.toUpperCase());
+    console.log(this.formSearch.value);
+
+    this.formSearch.reset()
+  }
+
 
 }
