4de2e92c81d407f20abda7e5ad6169b5a813da06
Eyder Suarez <ey_der19@hotmail.com>
2023-09-29 16:55:55 -0500

    Se avanza en el formulario de registro del formulario

--- src/app/components/form-material/form-material.component.ts
+++ src/app/components/form-material/form-material.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-material',
   templateUrl: './form-material.component.html',
   styleUrls: ['./form-material.component.css']
 })
-export class FormMaterialComponent {
+export class FormMaterialComponent implements OnInit {
+
+
+  public formMaterial!: FormGroup
+
+  constructor(private fb: FormBuilder) {}
+
+
+  ngOnInit() {
+
+    this.formMaterial = this.fb.group({
+      materialOrigen    : [""],
+      tipo              : [""],
+      paisExportador    : [""],
+      paisOrigen        : [""],
+      proveedor         : [""],
+      clasificacion     : [""],
+      materialLocal     : [""],
+      descripcion       : [""],
+      partidaArancelaria: [""],
+      upDate            : [new Date()]
+    })
+  }
+
+
+  enviar() {
+    console.log(this.formMaterial.value);
+    this.formMaterial.reset()
+  }
 
 }
